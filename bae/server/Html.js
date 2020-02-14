debc11ee23750d92494caa5f21a28ae5c1ab95b9
Praveen <mailtopraveen74@gmail.com>
2020-02-14 21:35:43 +0530

    added mern and bae

---
+++ bae/server/Html.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Html = props => 
+  <html>
+    <head>
+      <title>App!!!</title>
+    </head>
+    <body>
+      <h1>Test page12</h1>
+      <div id='node'/>
+      <script type="text/javascript" src="/client.js"></script>
+    </body>
+  </html>
+;
+
+export default Html;
